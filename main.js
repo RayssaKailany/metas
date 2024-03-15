eee557fcf3518f13c34b7554cd5269fb503d502c
Rayssa Kailany <102701353+RayssaKailany@users.noreply.github.com>
2024-03-15 11:25:25 -0300

    ok

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ for (let i= 0; i <botoes.length;i++){
     botoes[i].onclick = function(){
         for (let j=0; j <botoes.length;j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
+        texto[j].classList.add("ativo");
     }
-    console.log();
+    
 }
