84d8fbb4d8cf5ee2b9267fea8665ae79d2a0be60
Rayssa Kailany <102701353+RayssaKailany@users.noreply.github.com>
2024-04-01 10:33:57 -0300

    ok

--- main.js
+++ main.js
@@ -15,19 +15,20 @@ for (let i= 0; i <botoes.length;i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-31T00:00:00");
-let tempoAtual = new Date("2024-12-21T00:00:00");
+
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo1){
-    let tempoFinal = tempoObjetivo1 - tempoAtual;
-let segundos = Math.floor(tempoFinal/1000);
-let minutos = Math.floor(segundos/60);
-let horas = Math.floor(minutos/60);
-let dias = Math.floor(horas/24);
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
 
 segundos %= 60;
 minutos %= 60;
 horas %= 24;
 
-contadores[0].textContent = dias + " dias" + horas + " horas" + minutos + " minutos" + segundos + " segundos";
+return dias + " dias" + horas + " horas" + minutos + " minutos" + segundos + " segundos";
 }
