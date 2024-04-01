5824db43ee857fc4f4fa648e7c9fa1b09ac545da
Rayssa Kailany <102701353+RayssaKailany@users.noreply.github.com>
2024-04-01 10:24:37 -0300

    ok

--- main.js
+++ main.js
@@ -23,9 +23,9 @@ let tempoAtual = new Date("2024-12-21T00:00:00");
 //}
 
 let tempoFinal = tempoObjetivo1 - tempoAtual;
-let segundos = tempoFinal/1000;
-let minutos = segundos/60;
-let horas = minutos/60;
-let dias = horas/24;
+let segundos = Math.floor(tempoFinal/1000);
+let minutos = Math.floor(segundos/60);
+let horas = Math.floor(minutos/60);
+let dias = Math.floor(horas/24);
 
 contadores[0].textContent = dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
