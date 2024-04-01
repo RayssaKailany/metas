804ed5c4f12d0385422c4535dfc16f54152269cd
Rayssa Kailany <102701353+RayssaKailany@users.noreply.github.com>
2024-04-01 10:34:23 -0300

    ok

--- main.js
+++ main.js
@@ -15,7 +15,6 @@ for (let i= 0; i <botoes.length;i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-31T00:00:00");
-
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo1){
