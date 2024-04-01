f9bf400bc2e05e8c49a8422f9935b0510f89396f
Rayssa Kailany <102701353+RayssaKailany@users.noreply.github.com>
2024-04-01 10:00:52 -0300

    ok

--- main.js
+++ main.js
@@ -15,4 +15,5 @@ for (let i= 0; i <botoes.length;i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-31T00:00:00");
-contadores[0].textContent = tempoObjetivo1;
+let tempoAtual = new Date("2024-12-21T00:00:00");
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
