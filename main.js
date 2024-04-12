348c5f3a196b894eb03a2755eec824ce547382b1
Rayssa Kailany <102701353+RayssaKailany@users.noreply.github.com>
2024-04-12 10:53:32 -0300

    ok

--- main.js
+++ main.js
@@ -14,10 +14,10 @@ for (let i= 0; i <botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2024-12-05T00:00:00");
 const tempoObjetivo2 = new Date("2030-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2025-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-09-01T00:00:00");
+const tempoObjetivo4 = new Date("2024-12-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
